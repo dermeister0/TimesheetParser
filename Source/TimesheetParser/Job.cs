ead09a00a9c4708df0f3bcc137e4a718a280d460
Der_Meister <der@heavysoft.ru>
2015-02-12 22:07:17 +0700

    Add hash character to task numbers. Set minimal job duration to 1 minute.

--- Source/TimesheetParser/Job.cs
+++ Source/TimesheetParser/Job.cs
@@ -5,13 +5,19 @@ namespace TimesheetParser
     internal class Job
     {
         private const string DurationFormat = "{0:h:mm tt} - {1:h:mm tt} @ {2:hh}:{2:mm}";
-
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public string Task { get; set; }
         public string Description { get; set; }
 
-        public TimeSpan Duration => EndTime - StartTime;
+        public TimeSpan Duration
+        {
+            get
+            {
+                var duration = EndTime - StartTime;
+                return duration.TotalMinutes > 0 ? duration : TimeSpan.FromMinutes(1);
+            }
+        }
 
         public string TimeDescription => string.Format(DurationFormat, StartTime, EndTime, Duration);
     }
