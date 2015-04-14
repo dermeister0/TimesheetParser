c5f43d7d6ec4bd7c80b29f7dfa8988b4dad3a69e
Der_Meister <der@heavysoft.ru>
2015-04-15 02:00:46 +0700

    Make the interface public.

--- Source/Heavysoft.TimesheetParser.PluginInterfaces/ICrm.cs
+++ Source/Heavysoft.TimesheetParser.PluginInterfaces/ICrm.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace Heavysoft.TimesheetParser.PluginInterfaces
 {
-    internal interface ICrm
+    public interface ICrm
     {
         Task<bool> Login(string login, SecureString password);
         Task<bool> Login(string token);
