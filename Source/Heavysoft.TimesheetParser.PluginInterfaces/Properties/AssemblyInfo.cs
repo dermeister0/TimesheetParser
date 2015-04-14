78405c73f2ed6ca75b5472ef257a3bcbd5e94ca5
Der_Meister <der@heavysoft.ru>
2015-04-15 01:39:49 +0700

    Use strong name. Add NuGet config.

--- Source/Heavysoft.TimesheetParser.PluginInterfaces/Properties/AssemblyInfo.cs
+++ Source/Heavysoft.TimesheetParser.PluginInterfaces/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("Heavysoft.TimesheetParser.PluginInterfaces")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Interfaces to create plugins for TimesheetParser app.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Heavysoft")]
 [assembly: AssemblyProduct("Heavysoft.TimesheetParser.PluginInterfaces")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -17,7 +17,9 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("c5c5e5bb-04cc-4a6d-aee1-b8fad6658220")]
