43dfe521f4f7f473f13bb554f5206bb5ee331813
Der_Meister <der@heavysoft.ru>
2015-04-15 00:55:06 +0700

    Introduce Heavysoft.TimesheetParser.PluginInterfaces project.

---
+++ Source/BuildRelease.ps1
@@ -0,0 +1,11 @@
+﻿if ($PSVersionTable.PSVersion.Major -lt 3)
+{
+    throw "PowerShell 3 is required.`nhttp://www.microsoft.com/en-us/download/details.aspx?id=40855"
+}
+
+Import-Module "$PSScriptRoot\Scripts\BuildHelpers.psm1"
+
+Initialize-BuildVariables
+Invoke-NugetRestore "$PSScriptRoot\TimesheetParser.sln"
+
+Invoke-SolutionBuild "$PSScriptRoot\TimesheetParser.sln" 'Release'
---
+++ Source/Heavysoft.TimesheetParser.PluginInterfaces/ICrm.cs
@@ -0,0 +1,11 @@
+﻿using System.Security;
+using System.Threading.Tasks;
+
+namespace Heavysoft.TimesheetParser.PluginInterfaces
+{
+    internal interface ICrm
+    {
+        Task<bool> Login(string login, SecureString password);
+        Task<bool> Login(string token);
+    }
+}
