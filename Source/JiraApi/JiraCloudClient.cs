3703ef245acd53a1d0c4f2542b7604ad569f9c82
Der_Meister <der@heavysoft.ru>
2015-12-22 19:45:55 +0700

    Introduce JiraApi project.

---
+++ Source/JiraApi/JiraCloudClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Security;
+using System.Threading.Tasks;
+using Heavysoft.TimesheetParser.PluginInterfaces;
+
+namespace JiraApi
+{
+    class JiraCloudClient : ICrm
+    {
+        public Task<bool> Login(string login, SecureString password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Login(string token)
+        {
+            return Task.FromResult(false);
+        }
+
+        public Task<bool> AddJob(JobDefinition job)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TaskHeader> GetTaskHeader(string taskId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
