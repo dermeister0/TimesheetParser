6dbe9072417a8fac19fcd6494366b32bcf443cac
Der_Meister <der@heavysoft.ru>
2015-12-23 23:41:14 +0700

    Implement Login method.

--- Source/JiraApi/JiraCloudClient.cs
+++ Source/JiraApi/JiraCloudClient.cs
@@ -1,13 +1,41 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Threading.Tasks;
 using Heavysoft.TimesheetParser.PluginInterfaces;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace JiraApi
 {
-    class JiraCloudClient : ICrm
+    internal class ApiResult
     {
-        public Task<bool> Login(string login, string password)
+        public string Message { get; set; }
+        public string StatusCode { get; set; }
+    }
+
+    public class JiraCloudClient : ICrm
+    {
+        private const string ApiUrl = "https://saritasa.atlassian.net/rest/api/2/";
+
+        private string login;
+        private string password;
+        private RestClient restClient;
+
+        public JiraCloudClient()
         {
-            throw new System.NotImplementedException();
+            restClient = new RestClient(ApiUrl);
+        }
+
+        public async Task<bool> Login(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+
+            restClient.Authenticator = new HttpBasicAuthenticator(this.login, this.password);
+
+            var request = new RestRequest("myself");
+            var response = await restClient.ExecuteTaskAsync(request);
+
+            return response.ResponseStatus == ResponseStatus.Completed && response.StatusCode == HttpStatusCode.OK;
         }
 
         public Task<bool> Login(string token)
