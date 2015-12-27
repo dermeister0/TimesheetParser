14f78b82a528287d84a8dde560f3cd5d907aee43
Der_Meister <der@heavysoft.ru>
2015-12-27 14:50:06 +0700

    Dynamic plugin loading.

--- Source/TimesheetParser.Win10/Services/PluginService.cs
+++ Source/TimesheetParser.Win10/Services/PluginService.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Windows.Storage;
 using Heavysoft.TimesheetParser.PluginInterfaces;
-using JiraApi;
 using TimesheetParser.Business.Services;
 using TimesheetParser.Business.ViewModel;
 
@@ -19,12 +23,29 @@ namespace TimesheetParser.Win10.Services
         {
             var plugins = new List<CrmPluginViewModel>();
 
-            ICrm crmClient;
-            crmClient = new JiraCloudClient();
+            var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
 
-            var passwordService = new PasswordService();
-            var crmVM = new CrmPluginViewModel(crmClient, passwordService, dispatchService);
-            plugins.Add(crmVM);
+            List<Assembly> assemblies = new List<Assembly>();
+            foreach (StorageFile file in folder.GetFilesAsync().AsTask().Result)
+            {
+                if (file.FileType == ".dll" && file.Name.Contains("Api"))
+                {
+                    var fileName = file.Name.Substring(0, file.Name.Length - file.FileType.Length);
+                    Assembly assembly = Assembly.Load(new AssemblyName() { Name = fileName });
+                    var type = assembly.GetExportedTypes().FirstOrDefault(t => typeof(ICrm).IsAssignableFrom(t));
+
+                    if (type == null)
+                        continue;
+
+                    var crmClient = Activator.CreateInstance(type) as ICrm;
+                    if (crmClient == null)
+                        continue;
+
+                    var passwordService = new PasswordService();
+                    var crmVM = new CrmPluginViewModel(crmClient, passwordService, dispatchService);
+                    plugins.Add(crmVM);
+                }
+            }
 
             return plugins;
         }
