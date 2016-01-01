9280bd7568339cb7a626e7a1da412beff74153b2
Der_Meister <der@heavysoft.ru>
2016-01-01 18:44:01 +0700

    Switch to project.json for all projects.

--- Source/BuildRelease.ps1
+++ Source/BuildRelease.ps1
@@ -11,3 +11,15 @@ Invoke-NugetRestore "$PSScriptRoot\TimesheetParser.sln"
 $env:HVChangeset = (git 'rev-parse' 'HEAD').SubString(0, 7)
 
 Invoke-SolutionBuild "$PSScriptRoot\TimesheetParser.sln" 'Release'
+
+$homePath = $env:homedrive + $env:homepath
+$pluginsPath = "$PSScriptRoot\TimesheetParser\bin\Release\Plugins"
+
+if (!(Test-Path $pluginsPath))
+{
+    New-Item $pluginsPath -ItemType directory
+}
+
+# Copy JiraApi dependencies for WPF project.
+Copy-Item -Verbose "$homePath\.nuget\packages\FubarCoder.RestSharp.Portable.Core\3.1.0\lib\portable-net45+win+wpa81+wp80+MonoAndroid10+MonoTouch10+Xamarin.iOS10\RestSharp.Portable.Core.dll" $pluginsPath
+Copy-Item -Verbose "$homePath\.nuget\packages\FubarCoder.RestSharp.Portable.HttpClient\3.1.0\lib\net45\RestSharp.Portable.HttpClient.dll" $pluginsPath
