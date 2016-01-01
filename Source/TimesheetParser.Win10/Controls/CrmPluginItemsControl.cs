3bacf6cff4c0afc9e6539ecfd89fb40e0843941f
Der_Meister <der@heavysoft.ru>
2016-01-01 21:51:17 +0700

    Implement CrmPluginItemsControl control.

---
+++ Source/TimesheetParser.Win10/Controls/CrmPluginItemsControl.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace TimesheetParser.Win10.Controls
+{
+    public sealed class CrmPluginItemsControl : ItemsControl
+    {
+        public CrmPluginItemsControl()
+        {
+            this.DefaultStyleKey = typeof(CrmPluginItemsControl);
+        }
+    }
+}
