c9c24a9e4be0fdb50d910124188b8cd401161cba
Der_Meister <der@heavysoft.ru>
2016-01-01 19:35:57 +0700

    Copy SaritasaApi dependencies for WPF project. Introduce a wrapper project with all plugin dependencies for Win10 project.

--- Source/BuildRelease.ps1
+++ Source/BuildRelease.ps1
@@ -23,3 +23,8 @@ if (!(Test-Path $pluginsPath))
 # Copy JiraApi dependencies for WPF project.
 Copy-Item -Verbose "$homePath\.nuget\packages\FubarCoder.RestSharp.Portable.Core\3.1.0\lib\portable-net45+win+wpa81+wp80+MonoAndroid10+MonoTouch10+Xamarin.iOS10\RestSharp.Portable.Core.dll" $pluginsPath
 Copy-Item -Verbose "$homePath\.nuget\packages\FubarCoder.RestSharp.Portable.HttpClient\3.1.0\lib\net45\RestSharp.Portable.HttpClient.dll" $pluginsPath
+
+# Copy SaritasaApi dependencies for WPF project.
+Copy-Item -Verbose "$homePath\.nuget\packages\Newtonsoft.Json\7.0.1\lib\net45\Newtonsoft.Json.dll" $pluginsPath
+Copy-Item -Verbose "$homePath\.nuget\packages\PCLCrypto\1.0.86\lib\net40-Client\PCLCrypto.dll" $pluginsPath
+Copy-Item -Verbose "$homePath\.nuget\packages\Validation\2.0.6.15003\lib\portable-net40+sl50+win+wpa81+wp80+Xamarin.iOS10+MonoAndroid10+MonoTouch10\Validation.dll" $pluginsPath
