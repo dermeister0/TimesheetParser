c50401c8d5d296e5539e4ed00d5cb18161e56cc5
Der_Meister <der@heavysoft.ru>
2016-01-02 23:28:37 +0700

    Use transparent icons for start menu and splash screen. Add margins to icons. Set background color in manifest. Rename assembly.

--- Source/Scripts/index.js
+++ Source/Scripts/index.js
@@ -15,30 +15,39 @@ var fs = require('fs');
 
 var tempCounter = 0;
 
-function generateImage(fileName, width, height) {
+function generateImage(fileName, width, height, transparent) {
     var tempFile = 'temp' + ++tempCounter + '.png';
+    var iconSize = height * 0.66;
     
     gm('../../Images/Icon00.svg')
         .background('none')
         .density(600, 600)
         .negative()
-        .resize(height)
+        .resize(iconSize)
         .write(tempFile, function (err) {
             if (err) {
                 console.log(err);
                 return;
             }
             
-            var offset = (width - height) / 2;
+            var offsetX = (width - iconSize) / 2;
+            var offsetY = (height - iconSize) / 2;
             
-            gm(tempFile)
-                .background('#1593AC')
-                .extent(width, height, ' -' + offset)
+            var img = gm(tempFile);
+            
+            if (transparent) {
+                img.background('none');
+            } else {
+                img.background('#1593AC')
+            }
+                
+            img.extent(width, height, ` -${offsetX} -${offsetY}`)
                 .write(fileName, function (err) {
                     if (err) {
                         console.log(err);
                     }
                     
+                    console.log(`Updated ${fileName}.`);
                     fs.unlink(tempFile);
                 });
         });
@@ -46,10 +55,10 @@ function generateImage(fileName, width, height) {
 
 var outputDir = '../TimesheetParser.Win10/Assets/';
 
-generateImage(outputDir + 'LockScreenLogo.scale-200.png', 48, 48);
-generateImage(outputDir + 'SplashScreen.scale-200.png', 1240, 600);
-generateImage(outputDir + 'Square44x44Logo.scale-200.png', 88, 88);
-generateImage(outputDir + 'Square44x44Logo.targetsize-24_altform-unplated.png', 24, 24);
-generateImage(outputDir + 'Square150x150Logo.scale-200.png', 300, 300);
-generateImage(outputDir + 'StoreLogo.png', 50, 50);
-generateImage(outputDir + 'Wide310x150Logo.scale-200.png', 620, 300);
+generateImage(outputDir + 'LockScreenLogo.scale-200.png', 48, 48, false);
+generateImage(outputDir + 'SplashScreen.scale-200.png', 1240, 600, true);
+generateImage(outputDir + 'Square44x44Logo.scale-200.png', 88, 88, false);
+generateImage(outputDir + 'Square44x44Logo.targetsize-24_altform-unplated.png', 24, 24, false);
+generateImage(outputDir + 'Square150x150Logo.scale-200.png', 300, 300, true);
+generateImage(outputDir + 'StoreLogo.png', 50, 50, false);
+generateImage(outputDir + 'Wide310x150Logo.scale-200.png', 620, 300, true);
