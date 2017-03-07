17b42c23ca4fffbc1169a01feb20031781e7ad6a
Anton Zimin <anton@saritasa.com>
2017-03-07 18:26:34 +0700

    Implement Skip button for UWP #4

---
+++ Source/TimesheetParser.Win10/Support/BooleanToVisibilityNegativeConverter.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml;
+
+namespace TimesheetParser.Win10.Support
+{
+    public sealed class BooleanToVisibilityNegativeConverter : BooleanConverter<Visibility>
+    {
+        public BooleanToVisibilityNegativeConverter() :
+            base(Visibility.Collapsed, Visibility.Visible)
+        {
+        }
+    }
+}
