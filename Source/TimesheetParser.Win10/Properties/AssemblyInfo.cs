4fb9f359f877935098c17cec284942d987063ebc
Anton Zimin <anton@saritasa.com>
2017-05-10 17:49:53 +0700

    Create nuspec for app, introduce release-app task

--- Source/TimesheetParser.Win10/Properties/AssemblyInfo.cs
+++ Source/TimesheetParser.Win10/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Heavysoft")]
 [assembly: AssemblyProduct("Timesheet Parser")]
-[assembly: AssemblyCopyright("Copyright © Der_Meister 2015-2016")]
+[assembly: AssemblyCopyright("Copyright © Der_Meister 2015-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
