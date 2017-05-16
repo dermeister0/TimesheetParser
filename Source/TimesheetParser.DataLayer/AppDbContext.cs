d65b208cd09937395f8ff44fbf29a2ce4a811224
Anton Zimin <anton@saritasa.com>
2017-05-16 22:02:54 +0700

    Introduce TimesheetParser.DataLayer project #20

---
+++ Source/TimesheetParser.DataLayer/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimesheetParser.DataLayer.Entities;
+
+namespace TimesheetParser.DataLayer
+{
+    /// <summary>
+    /// Main app DB context.
+    /// </summary>
+    internal class AppDbContext : DbContext
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="options"></param>
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// Timesheets DB set.
+        /// </summary>
+        public DbSet<Timesheet> Timesheets { get; set; }
+    }
+}
---
+++ Source/TimesheetParser.DataLayer/Entities/Timesheet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimesheetParser.DataLayer.Entities
+{
+    /// <summary>
+    /// Timesheet entity.
+    /// </summary>
+    public class Timesheet
+    {
+        /// <summary>
+        /// Date of timesheet.
+        /// </summary>
+        [Key]
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Raw timesheet text.
+        /// </summary>
+        public string Text { get; set; }
+    }
+}
