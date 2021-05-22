718a6d2a0dd914b498eb8f24da4bfeef0c821a1e
Anton Zimin <anton@saritasa.com>
2021-05-21 23:00:23 -0700

    Use regex to find idle jobs #31

--- Source/TimesheetParser.Business/IdleStrategies/BaseIdleStrategy.cs
+++ Source/TimesheetParser.Business/IdleStrategies/BaseIdleStrategy.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace TimesheetParser.Business.IdleStrategies
 {
     abstract class BaseIdleStrategy : IIdleStrategy
     {
+        Regex idleRegex = new Regex(@"(?i)^\s?idle\.?\s?$", RegexOptions.Compiled);
+
         public abstract void DistributeIdle(ParseResult result);
 
         protected IList<Model.Job> GetIdleJobs(ParseResult result)
         {
-            return result.Jobs.Where(j => string.Compare(j.Description.Trim(), "Idle.", StringComparison.OrdinalIgnoreCase) == 0).ToList();
+            return result.Jobs.Where(j => idleRegex.IsMatch(j.Description.Trim())).ToList();
         }
     }
 }
